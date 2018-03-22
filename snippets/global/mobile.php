7a3e320cd39a12d1fdb3ccf2af5c57d50fc60ab0
Juan Pablo Solano <juanpa_solano@hotmail.com>
2018-03-22 18:43:15 -0500

    removes unused code

--- snippets/global/mobile.php
+++ snippets/global/mobile.php
@@ -1,11 +1,3 @@
-<?php
-global $woocommerce;
-$items  = $woocommerce->cart->get_cart_contents_count();
-$user   = wp_get_current_user();
-$first  = $user->user_firstname;
-$logout = esc_url(wc_logout_url(wc_get_page_permalink('myaccount')));
-?>
-
 <ul class="side-nav" id="sidebar-nav">
     <li>
         <div class="user-view">
@@ -25,33 +17,4 @@ $logout = esc_url(wc_logout_url(wc_get_page_permalink('myaccount')));
         "menu_class"=> "",
         'walker' => new Main_Mobile_Nav_Walker
     )) ?>
-
-    <!--         
-    <?php if (current_user_can('edit')) { ?>
-        <li><a href="/quick-cart">
-            <i class="material-icons left">shopping_cart</i>
-            Cart (<span id="cart-count"><?php echo $items; ?></span>)
-        </a></li>
-    <?php } ?>
-
-    <li id="sidebar-user-account" class="baseline flex">
-        <?php if (is_user_logged_in()) { ?>
-            <a id="username" href="/my-account">
-                <i class="material-icons left">person</i>
-                <?php
-                if (!empty($first)) {
-                    echo "Hi ".$first;
-                } else {
-                    echo "My Account";
-                }
-                ?>
-            </a>
-            <form id="logout" action="logout" method="post">
-                <?php wp_nonce_field( 'ajax-logout-nonce', 'logoutSecurity' ); ?>
-                <button type="submit" name="submit">(logout)</button>
-            </form> 
-        <?php } else { ?>
-            <a id="login" class="modal-trigger" href="#loginModal">Log In</a>
-        <?php } ?>
-    </li> -->
 </ul>
